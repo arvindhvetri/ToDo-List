dcea6495a2f481642d08638cf2c56cd440da2a20
Arvindh Kumar V <arvindhsharmi@gmail.com>
2025-04-26 01:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def toggle(task_id):
     return redirect('/') # Redirect back to the main page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
